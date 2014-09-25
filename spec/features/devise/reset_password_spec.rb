4246213eba138fa03f573abc7588dd5db175dc5c
bahelms <jimbonkgit@gmail.com>
2014-09-25 06:27:57 -0400

    adds reset password tests

--- spec/features/devise/reset_password_spec.rb
+++ spec/features/devise/reset_password_spec.rb
@@ -10,11 +10,27 @@ feature "Reseting password" do
     click_link "Forgot your password?"
   end
 
-  scenario "sends the user an email with a reset password link" do
-    fill_in "Email", with: user.email
-    expect {
+  context "with valid email address" do
+    background { fill_in "Email", with: user.email }
+
+    scenario "sends the user an email with a reset password link" do
+      expect {
+        click_button "Send reset password instructions"
+      }.to change(ActionMailer::Base.deliveries, :count).by(1)
+    end
+
+    scenario "sends an email to the address provided" do
       click_button "Send reset password instructions"
-    }.to change(ActionMailer::Base.deliveries, :count).by(1)
+      expect(mail.to).to eq [user.email]
+    end
+  end
+
+  context "with nonexistant email address" do
+    scenario "displays error message" do
+      fill_in "Email", with: "heybobbyjoe@dotcom.com"
+      click_button "Send reset password instructions"
+      expect(subject).to have_content("Email not found")
+    end
   end
 end
 
